d214432935cc888ad5edb8313c51305724137326
DeepCleansCanada <johnny@deep-cleans.com>
2026-04-01 02:38:23 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Deep Cleans Routing App</h1>
+      <p>System is live 🚀</p>
+    </div>
+  );
+}
